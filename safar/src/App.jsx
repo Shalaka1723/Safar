681e3fa310fe5ee3e2062199ec4c81b651d2ecca
Shalaka <shalakaw17@gmail.com>
2024-08-22 01:07:45 +0530

    header

--- safar/src/App.jsx
+++ safar/src/App.jsx
@@ -1,13 +1,21 @@
-import './App.css'
+import "./App.css";
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
 
 function App() {
-
   return (
-    <>
-    <div>fffff</div>
- 
-    </>
-  )
+
+      <div>
+        <header className="p-4">
+          <a href="" className="flex items-center gap-1">
+            <AirplanemodeActiveIcon/>
+
+
+            <span>safar</span>
+          </a>
+        </header>
+      </div>
+
+  );
 }
 
-export default App
+export default App;
