0c24a62bfc02d92cc05f87d35738063e3e81aec6
Shalaka <shalakaw17@gmail.com>
2024-08-25 01:18:05 +0530

    api

--- safarstay/src/pages/Register.jsx
+++ safarstay/src/pages/Register.jsx
@@ -1,32 +1,50 @@
-
-import { Link } from 'react-router-dom';
-import Header from '../Header';
-
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 function Register() {
-    const[name,setName] = useState('');
-    const[email,setEmail] = useState('');
-    const[password,setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let registerUser=(event)=>{
+    event.preventDefault();
+    axios.get('http://localhost:4000/test');
+  }
 
   return (
-    <div className='mt-4 grow flex items-center h-screen justify-around'>
-      <div className='mb-64'>
-      <h1 className='text-4xl mb-4 text-center'>Register</h1>
-        <form className='max-w-md mx-auto'>
-          <input type='text' placeholder='Your Name'/>
-          <input type='email' placeholder='Your Email'/>
-          <input type='password' placeholder='Password'/>
-          <button className='primary'>Login</button>
-          <div className='text-center py-2  text-gray-400'>
+    <div className="mt-4 grow flex items-center h-screen justify-around">
+      <div className="mb-64">
+        <h1 className="text-4xl mb-4 text-center">Register</h1>
+        <form className="max-w-md mx-auto" onSubmit={registerUser()}>
+          <input
+            type="text"
+            placeholder="Your Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button className="primary">Login</button>
+          <div className="text-center py-2  text-gray-400">
             Already a member?
-            <Link className='underline text-black' to={'/login'}>Login</Link>
+            <Link className="underline text-black" to={"/login"}>
+              Login
+            </Link>
           </div>
         </form>
-
       </div>
     </div>
-
-  )
+  );
 }
 
-export default Register
+export default Register;
