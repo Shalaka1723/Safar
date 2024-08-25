2bd68dee2cefc791f7dd3367120c1f6d6b8e7b3d
Shalaka <shalakaw17@gmail.com>
2024-08-25 18:54:48 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: '',
+    
+}));
 
 app.get('/test',(req,res)=>{
     res.json('test okay')
